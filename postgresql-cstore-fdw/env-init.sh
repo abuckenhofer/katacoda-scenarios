8121b4a53cc5533c5011cd9a46582ad113ab6e2a
Andreas Buckenhofer <andreas.buckenhofer@gmail.com>
2021-01-23 18:02:55 +0100

    changed docker run

--- postgresql-cstore-fdw/env-init.sh
+++ postgresql-cstore-fdw/env-init.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/bash
 
+# run in forground
+
 docker run -d -p 5432:5432 -e POSTGRES_PASSWORD=postgres --name columnarpostgresql abuckenhofer/columnarpostgresql:v1.3
 docker exec -it columnarpostgresql bash
+
+psql
