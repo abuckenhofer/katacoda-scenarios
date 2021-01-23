c061691f40685e4f562d1d85814fee033f8406c7
Andreas Buckenhofer <andreas.buckenhofer@gmail.com>
2021-01-23 17:13:41 +0100

    changed docker run

--- environment/postgresql01/build/01_pulldockerimages.sh
+++ environment/postgresql01/build/01_pulldockerimages.sh
@@ -1,1 +1,1 @@
-docker run -d -p 5432:5432 --name columnarpostgresql abuckenhofer/columnarpostgresql:v1.3
+docker run -d -p 5432:5432 -e POSTGRES_PASSWORD=postgres --name columnarpostgresql abuckenhofer/columnarpostgresql:v1.3
