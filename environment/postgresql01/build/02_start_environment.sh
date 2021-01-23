b871bcb998b549bf6674e909e8dcecd205647be0
Andreas Buckenhofer <andreas.buckenhofer@gmail.com>
2021-01-23 16:35:26 +0100

    changed docker run

--- environment/postgresql01/build/02_start_environment.sh
+++ environment/postgresql01/build/02_start_environment.sh
@@ -1,3 +1,5 @@
-docker pull abuckenhofer/columnarpostgresql:1.3
+docker run -d -p 5432:5432 --name columnarpostgresql abuckenhofer/columnarpostgresql:v1.3
 
-docker run -it -d -p 5432:5432 --name columnarpostgresql
+docker exec -it columnarpostgresql bash
+
+
